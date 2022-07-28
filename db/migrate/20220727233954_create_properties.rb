c8ca06f82346d917d9152c7d6a763941e4006700
Shiva Bhusal <hotline.shiva@gmail.com>
2022-07-28 06:40:40 +0545

    fixes table name in db-view

--- db/migrate/20220727233954_create_properties.rb
+++ db/migrate/20220727233954_create_properties.rb
@@ -1,18 +1,18 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
-    create view all_properties as
-      select id, owner, address, sqmt, price, floors, air_cond, rooms, 'House' as kind, null as parking, null as shops, null as units, created_at, updated_at from houses
-      union
-      select id, owner, address, sqmt, price, null as floors, null as air_cond, null as rooms,  'CommercialUnit' as kind, parking, shops, null as units,  created_at, updated_at from commercial_units
-      union
-      select id, owner, address, sqmt, price, null as floors, null as air_cond, null as rooms, 'ComplexBuilding' as kind,  null as parking, null as shops, units,  created_at, updated_at from complex_buildings
+      create view properties as
+        select id, owner, address, sqmt, price, floors, air_cond, rooms, 'House' as kind, null as parking, null as shops, null as units, created_at, updated_at from houses
+        union
+        select id, owner, address, sqmt, price, null as floors, null as air_cond, null as rooms,  'CommercialUnit' as kind, parking, shops, null as units,  created_at, updated_at from commercial_units
+        union
+        select id, owner, address, sqmt, price, null as floors, null as air_cond, null as rooms, 'ComplexBuilding' as kind,  null as parking, null as shops, units,  created_at, updated_at from complex_buildings
     SQL
   end
 
   def down
     execute <<-SQL
-    drop view all_properties
+      drop view properties
     SQL
   end
 end
