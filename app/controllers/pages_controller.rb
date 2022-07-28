6b6fc6d6e799fc46976ce7f172d3d4f779f4ada4
Shiva Bhusal <hotline.shiva@gmail.com>
2022-07-28 06:40:20 +0545

    Displays list of properties in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @assets = Property.order(:updated_at).page(params[:page])
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,11 @@
 class Property < ApplicationRecord
+  paginates_per 9
 
+  def readonly?
+    true
+  end
+
+  def pictures
+    ActiveStorage::Attachment.where(record_id: id, record_type: kind)
+  end
 end
