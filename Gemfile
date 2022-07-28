c12d5a45d9804dd6f8f9e5f076cdd0a7374d9a91
Shiva Bhusal <hotline.shiva@gmail.com>
2022-07-28 07:08:46 +0545

    Installs Administrate for managers to manage properties

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
-
+gem "administrate"
+gem 'administrate-field-active_storage'
 gem 'kaminari'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :houses
+      resources :complex_buildings
+      resources :commercial_units
+      root to: "houses#index"
+    end
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
